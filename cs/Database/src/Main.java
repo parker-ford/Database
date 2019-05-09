4ea5ea9fb3331848b50f2a7fab5dc6ced25a2c1f
parke <fordp2@wwu.edu>
2019-05-08 17:52:15 -0700

    javafx

--- cs/Database/src/Main.java
+++ cs/Database/src/Main.java
@@ -1,7 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
     public static void main(String[] args){
+        System.out.println("test");
+        launch(args);
         JDBC jdbc = new JDBC(args[0], args[1]);
         jdbc.run();
+
+
     }
+
+
 }
