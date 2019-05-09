753d1924204f5b967517a4abc05711724cef16a2
parke <fordp2@wwu.edu>
2019-05-08 20:00:37 -0700

    testing user input

--- cs/Database/src/loginController.java
+++ cs/Database/src/loginController.java
@@ -1,8 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class loginController {
+
+    @FXML
+    public Button submitButton;
+    @FXML
+    public TextField studentIDText;
 
     private String userID;
     private String password;
@@ -10,23 +22,21 @@ public class loginController {
     private final String preConnectionString = "jdbc:mysql://mysql.cs.wwu.edu:3306/";
     private final String postConnectionString = "?useSSL=false";
 
-    public loginController(){
+    public loginController() {
         System.out.println("login");
-
 
 
     }
 
-    public void initData(String[] loginInfo){
+    public void initData(String[] loginInfo) {
         System.out.println("data init");
         this.userID = loginInfo[0];
         this.password = loginInfo[1];
 
-        try{
+        try {
             Connection connection = getConnection();
             System.out.println("successful connection");
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -37,8 +47,14 @@ public class loginController {
                 password);
     }
 
-    private String getConnectionString(){
+    private String getConnectionString() {
         return preConnectionString + userID + postConnectionString;
     }
 
+
+    public void handleSubmit(ActionEvent event) throws IOException, ParserConfigurationException {
+        String studentID = studentIDText.getText();
+        System.out.println(studentID);
+
+    }
 }
